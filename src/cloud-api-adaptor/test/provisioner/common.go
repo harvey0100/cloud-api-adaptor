bc0741593d8d3c00f76d054389b9bb5a2aa3f321
stevenhorsman <steven@uk.ibm.com>
2024-04-30 13:54:12 +0100

    test/e2e: Add image cleanup workaround
    Due to an issue with containerd not caching images with snapshotter labels
    and peer-pods using the nydus-snapshotter to implement the image pull on
    guest, we see failures do to the pause image (and potentially other test
    images) having been pulled with overlayfs. This means that nydus-snapshotter
    doesn't trigger the guest_pull code, so to get this working, we first need
    to clean up images that have already been cached locally. For the CI I've
    written a debug container that does this, so run it after provisioning of
    the cluster and hopefully the tests will then pass.
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- src/cloud-api-adaptor/test/provisioner/common.go
+++ src/cloud-api-adaptor/test/provisioner/common.go
@@ -4,14 +4,22 @@
 package provisioner
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"strings"
+	"time"
 
+	log "github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/e2e-framework/klient"
 	"sigs.k8s.io/e2e-framework/klient/k8s"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 )
 
@@ -52,5 +60,44 @@ func AddNodeRoleWorkerLabel(ctx context.Context, clusterName string, cfg *envcon
 		}
 
 	}
+	return nil
+}
+
+func GetPodLog(ctx context.Context, client klient.Client, pod corev1.Pod) (string, error) {
+	clientset, err := kubernetes.NewForConfig(client.RESTConfig())
+	if err != nil {
+		return "", err
+	}
+
+	req := clientset.CoreV1().Pods(pod.ObjectMeta.Namespace).GetLogs(pod.ObjectMeta.Name, &corev1.PodLogOptions{})
+	podLogs, err := req.Stream(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer podLogs.Close()
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, podLogs)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+func DeletePod(ctx context.Context, client klient.Client, pod *corev1.Pod, deleteDuration *time.Duration) error {
+	duration := 1 * time.Minute
+	if deleteDuration == nil {
+		deleteDuration = &duration
+	}
+	if err := client.Resources().Delete(ctx, pod); err != nil {
+		return err
+	}
+	log.Infof("Deleting pod %s...", pod.Name)
+	if err := wait.For(conditions.New(
+		client.Resources()).ResourceDeleted(pod),
+		wait.WithInterval(5*time.Second),
+		wait.WithTimeout(*deleteDuration)); err != nil {
+		return err
+	}
+	log.Infof("Pod %s has been successfully deleted within %.0fs", pod.Name, deleteDuration.Seconds())
 	return nil
 }
